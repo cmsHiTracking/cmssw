df4e29557abff176e4b60dbfc3c46de2d3d4a12c
abaty <abaty@mit.edu>
2017-08-29 19:06:02 +0200

    same as pr 20303

--- RecoHI/HiTracking/python/hiDetachedTripletStep_cff.py
+++ RecoHI/HiTracking/python/hiDetachedTripletStep_cff.py
@@ -20,8 +20,11 @@ hiDetachedTripletStepClusters = cms.EDProducer("HITrackClusterRemover",
      )
 )
 
-
-
+from Configuration.Eras.Modifier_trackingPhase1_cff import trackingPhase1
+trackingPhase1.toModify(hiDetachedTripletStepClusters,
+     trajectories = cms.InputTag("hiDetachedQuadStepTracks"),
+     overrideTrkQuals = cms.InputTag("hiDetachedQuadStepSelector","hiDetachedQuadStep"),
+)
 
 # SEEDING LAYERS
 import RecoTracker.TkSeedingLayers.PixelLayerTriplets_cfi
@@ -109,7 +112,6 @@ hiDetachedTripletStepPixelTracks = cms.EDProducer("PixelTrackProducer",
     # Cleaner
     Cleaner = cms.string("trackCleaner")
 )
-from Configuration.Eras.Modifier_trackingPhase1_cff import trackingPhase1
 trackingPhase1.toModify(hiDetachedTripletStepPixelTracks,
     SeedingHitSets = cms.InputTag("hiDetachedTripletStepTracksHitTripletsCA")
 )
